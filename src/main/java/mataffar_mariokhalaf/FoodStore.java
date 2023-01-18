3240270bac57e9699522cedc6c97aa8e27c097bc
Mario Khalaf <Mario.khalaf9810@gmail.com>
2023-01-18 21:42:35 +0100

    Added json file &  methods to read, write & update json file

--- src/main/java/mataffar_mariokhalaf/FoodStore.java
+++ src/main/java/mataffar_mariokhalaf/FoodStore.java
@@ -1,35 +1,98 @@
 package mataffar_mariokhalaf;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class FoodStore {
-    private Inventory inventory;
+class FoodStore {
+    private File productsJson;
+    private List<Product> products;
+    private List<Product> basket;
 
     public FoodStore() {
-        inventory = new Inventory();
+        products = new ArrayList<>();
+        productsJson = new File("target/products.json");
+        loadProductsFromJson();
     }
 
-    public void addProduct(Product product) {
-        inventory.addProduct(product);
+
+    public void loadProductsFromJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Map<String, List<Product>> map = objectMapper.readValue(productsJson, 
+            new TypeReference<Map<String, List<Product>>>(){});
+            products = map.get("Product");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void removeProduct(Product product) {
-        inventory.removeProduct(product);
+    public void saveProductsToJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Map<String, List<Product>> map = new HashMap<>();
+            map.put("Product", products);
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(productsJson, map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public int getInventoryCount() {
-        return inventory.getProductCount();
+    public void addProductToInventory(Product product) {
+        products.add(product);
+        saveProductsToJson();
     }
 
-    public float checkOut(Customer customer) {
-        return customer.getTotalBill();
-    }    
+    public void removeProductFromInventory(Product product) {
+        products.remove(product);
+        saveProductsToJson();
+    }
 
-    public List<Product> getAllProducts() {
-        return inventory.getAllProducts();
+    public int checkInventoryCount() {
+        return products.size();
+    }
+
+    public void browseProducts() {
+        for (Product product : products) {
+            System.out.println(product);
+        }
     }
     
-    public void decreaseProductQuantity(Product product){
-        inventory.decreaseProductQuantity(product);
+    public Product getProductByName(String name) {
+        for(Product product : products) {
+            if(product.getName().equals(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void addToBasket(Product product, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            basket.add(product);
+        }
+    }
+    
+    public List<Product> getBasket() {
+        return basket;
+    }
+
+    public void decreaseQuantity(Product product, int quantity) {
+        for (Product p : products) {
+            if (p.getName().equals(product.getName())) {
+                if (p.getQuantity() >= quantity) {
+                    p.setQuantity(p.getQuantity() - quantity);
+                    return;
+                } else {
+                    throw new IllegalArgumentException("The product is out of stock");
+                }
+            }
+        }
+        throw new IllegalArgumentException("Product not found in inventory");
     }
 }
